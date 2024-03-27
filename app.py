ccf4d761d6d4f7a532c5fc3aaeec0e19061a7d44
Devashri Deulkar <Devashri@users.noreply.huggingface.co>
2024-03-27 06:25:27 +0000

    Update app.py
    add if statement

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ with col2:
 #     st.pyplot(fig)
 # st.success('Model executed successfully with user input and slider values.')
 
+if st.button('Run Model and Generate Plots'):
 # Call the modified run_sir_model function with user input and slider values
 fig,gif_path = run_sir_model(total_population, initial_infected, initial_recovered, beta, gamma, mode="run")
 st.write("Static Population Model Output:")
